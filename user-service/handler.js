d5e70858d2d6027ccb5af3eb97201b525e270afc
Mario <mario.martinez@secretescapes.com>
2018-11-30 11:21:35 +0100

    sending msg to responses topic

--- user-service/handler.js
+++ user-service/handler.js
@@ -19,13 +19,62 @@ async function processMessage(message) {
   let dynamodb = new AWS.DynamoDB(dynamoDBOpts);
   var params = prepareItem(slackUsername, githubUsername, tableName);
 
+  var error;
   try {
     console.log(`Storing item: ${JSON.stringify(params)}`);
     await dynamodb.putItem(params).promise();
     console.log(`Item stored`);
-    //TODO: Send message with error/success
   } catch (err) {
     console.error(`Error storing to dynamodb: ${err}`);
+    error = err;
+  }
+
+  try {
+    if (error) {
+      await onError(message, err);
+    } else {
+      await onSuccessful(message);
+    }
+  } catch (err) {
+    console.err(`Error sending msg: ${err}`);
+  }
+}
+
+async function onError(message, error) {
+  const newMessage = Object.assign(
+    { notificationType: "REGISTER_ERROR", error },
+    message
+  );
+  await publishEvent(prepareEventMessage(newMessage));
+}
+async function onSuccessful(message) {
+  const newMessage = Object.assign(
+    { notificationType: "REGISTER_SUCCESS" },
+    message
+  );
+  await publishEvent(prepareEventMessage(newMessage));
+}
+
+function prepareEventMessage(messageJson) {
+  return {
+    Message: JSON.stringify(messageJson),
+    TopicArn: process.env.responsesTopicArn
+  };
+}
+
+// TODO: This can be extracted to a commons module
+async function publishEvent(messageData) {
+  console.info(`Publising message: ${JSON.stringify(messageData)}`);
+  let snsOpts = {
+    region: process.env.myRegion
+  };
+  let sns = new AWS.SNS(snsOpts);
+  try {
+    await sns.publish(messageData).promise();
+    console.info("message published");
+  } catch (err) {
+    console.error(JSON.stringify(err));
+    throw new Error("Error publishing event");
   }
 }
 
